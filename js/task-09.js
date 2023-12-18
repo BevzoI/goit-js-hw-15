a7ea7c52bf662d8e53418dc6ac27fba1dcc09316
BevzoI <bevzo.i.n@gmail.com>
2023-12-18 08:45:33 +0100

    task 6

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const colorSpan = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const changeColorAction = function () {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+}
+
+changeColorBtn.addEventListener('click', changeColorAction);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
